f82a8fb2be6587d4076766a443861b65b59e266c
Ilya Pcn <pchelinilya13@gmail.com>
2023-01-22 15:47:05 +1100

    feat: add main func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,2 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse (n) { return parseInt(n.toString().split('').reverse().join('')) }
+// я уже делал это задание на stage 0, набор декабрь 21го
